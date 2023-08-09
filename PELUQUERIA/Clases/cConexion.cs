3ec066e6a20d267141bb723153a8ec0974250576
Pablo <44281098+pablomartinzabala2@users.noreply.github.com>
2023-08-09 22:19:50 +0200

    ass
    sss

--- PELUQUERIA/Clases/cConexion.cs
+++ PELUQUERIA/Clases/cConexion.cs
@@ -10,9 +10,10 @@ namespace PELUQUERIA.Clases
         public static string Cadenacon()
         {
             //*****CASA**********
-              string cadena = "Data Source=DESKTOP-QKECIIE;Initial Catalog=PL;Integrated Security=True";
-          //  string cadena = "Data Source=DESKTOP-HE27MBV\\PABLO;Initial Catalog=PELUQUERIA;Integrated Security=True";
-           
+            //  string cadena = "Data Source=DESKTOP-QKECIIE;Initial Catalog=PL;Integrated Security=True";
+            //  string cadena = "Data Source=DESKTOP-HE27MBV\\PABLO;Initial Catalog=PELUQUERIA;Integrated Security=True";
+            string cadena = PELUQUERIA.Properties.Settings.Default.PELUQUERIAConnectionString;
+
             return cadena;
         }
     }
